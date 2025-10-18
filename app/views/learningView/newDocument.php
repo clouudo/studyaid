7dac1a05f3bc6dd5e80ba271299845648083fd48
yeohmantik <mantik3333@gmail.com>
2025-10-18 20:54:16 +0800

    UI Changes

--- app/views/learningView/newDocument.php
+++ app/views/learningView/newDocument.php
@@ -7,8 +7,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/style.css">
 </head>
-<body>
-    <div class="d-flex">
+<body class="d-flex flex-column min-vh-100">
+    <div class="d-flex flex-grow-1">
         <?php include 'app\views\sidebar.php'; ?>
         <main class="flex-grow-1 p-3">
             <div class="container">
@@ -30,7 +30,7 @@
                     <div class="mb-3">
                         <label for="dragDropArea" class="form-label">Drag and Drop Document</label>
                         <div id="dragDropArea" class="border rounded p-5 text-center bg-light d-flex flex-column justify-content-center align-items-center" style="min-height: 550px; cursor: pointer;" onclick="document.getElementById('documentFile').click();">
-                            Drag and drop your files here or click to upload
+                            <span id="fileNameDisplay">Drag and drop your files here or click to upload</span>
                             <input type="file" id="documentFile" name="document" style="display: none;">
                             <p class="mt-3">Or</p>
                             <button type="button" class="btn btn-outline-primary" onclick="document.getElementById('documentFile').click();">Browse Files</button>
@@ -43,5 +43,15 @@
         </main>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script>
+        document.getElementById('documentFile').addEventListener('change', function() {
+            const fileNameDisplay = document.getElementById('fileNameDisplay');
+            if (this.files && this.files.length > 0) {
+                fileNameDisplay.textContent = this.files[0].name;
+            } else {
+                fileNameDisplay.textContent = 'Drag and drop your files here or click to upload';
+            }
+        });
+    </script>
 </body>
 </html>
